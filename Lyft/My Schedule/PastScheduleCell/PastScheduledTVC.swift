1e11bc66d805e4a4ca2ae49bfc5aee5193ed2a3e
Diwakar Garg <diwakar@iatocs.com>
2022-12-07 13:23:52 +0530

    Added the past ScheduledTVC

---
+++ Lyft/My Schedule/PastScheduleCell/PastScheduledTVC.swift
@@ -0,0 +1,48 @@
+//
+//  PastScheduledTVC.swift
+//  Lyft
+//
+//  Created by Diwakar Garg on 21/11/22.
+//
+
+import UIKit
+
+class PastScheduledTVC: UITableViewCell {
+    
+    class var identifier: String { return String(describing: self) }
+    class var nib: UINib { return UINib(nibName: identifier, bundle: nil) }
+    
+    @IBOutlet weak var classImageView: UIImageView!
+    @IBOutlet weak var classNameLabel: UILabel!
+    @IBOutlet weak var classNameValueLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dateValueLabel: UILabel!
+    @IBOutlet weak var dayValueLabel: UILabel!
+    @IBOutlet weak var instructorLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var instructorValueLabel: UILabel!
+    @IBOutlet weak var durationValueLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var endTimeLabel: UILabel!
+    @IBOutlet weak var startTimeValueLabel: UILabel!
+    @IBOutlet weak var endTimeValueLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        dateLabel.text = Constants.dateLabel.localize()
+        dayLabel.text = Constants.dayLabel.localize()
+        durationLabel.text = Constants.durationLabel.localize()
+        instructorLabel.text = Constants.instructorLabel.localize()
+        startTimeLabel.text = Constants.startTimeLabel.localize()
+        endTimeLabel.text = Constants.endTimeLabel.localize()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
